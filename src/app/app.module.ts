cc0acbcd3fa44509f456ebecc262dd0efc984f0b
Aditya <adityapawar826@gmail.com>
2025-06-20 19:43:02 +0530

    components and its content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateTaskComponent } from './dashboard/create-task/create-task.component';
+import { TaskDetailComponent } from './dashboard/task-detail/task-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    DashboardComponent,
+    CreateTaskComponent,
+    TaskDetailComponent
   ],
   imports: [
     BrowserModule,
