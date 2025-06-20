c49e10880c351a2b925c65a0fce3433c9e19b079
Aditya <adityapawar826@gmail.com>
2025-06-20 21:31:24 +0530

    Get call with transormed data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,16 +1,21 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Task } from '../Model/task';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 http:HttpClient=inject(HttpClient);
 
 showCreateTaskForm: boolean = false;
+
+ngOnInit(): void {
+  this.fetchAllTasks();
+}
 
   OpenCreateTaskForm(){
     this.showCreateTaskForm = true;
@@ -24,5 +29,30 @@ showCreateTaskForm: boolean = false;
     this.http.post('https://http-request-9281e-default-rtdb.firebaseio.com/tasks.json',data,{headers:headers}).subscribe((res)=>{
       console.log(res);
     });
+  }
+
+/*
+{
+  key:{},
+  key:{}
+}
+*/
+  private fetchAllTasks(){
+    this.http.
+    get<{[key:string]:Task}>('https://http-request-9281e-default-rtdb.firebaseio.com/tasks.json')
+    .pipe(map((response)=>{
+      let task=[];
+
+      for(let key in response){
+        if(response.hasOwnProperty(key)){
+
+          task.push({...response[key],id:key})
+        }
+      }
+      return task;
+    }))
+    .subscribe((task)=>{
+      console.log(task)
+    })
   }
 }
